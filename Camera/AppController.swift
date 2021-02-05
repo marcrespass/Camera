7897248cf5f2bddf1ff3f6005ad0ae6a2600402e
Marc Respass <marc@iliosinc.com>
2021-02-05 13:38:44 -0500

    New Xcode! Changed from window controllers to windows and save the window frame

--- Camera/AppController.swift
+++ Camera/AppController.swift
@@ -11,31 +11,38 @@ private extension Selector {
 final class AppController {
 
     let contentVC = ContentVC()
-    var windowControllers = [NSWindowController]()
+    var window: NSWindow?
 
     @objc func createNewWindow() {
+        let frameDescriptor = UserDefaults.standard.value(forKey: "CameraWindowFrame") as? String
+
         let window = NSWindow(contentViewController: self.contentVC)
         window.title = NSLocalizedString("Camera", comment: "")
         window.tabbingMode = .disallowed
-        let wc = NSWindowController(window: window)
-        self.windowControllers.append(wc)
 
-        wc.window?.center()
-        wc.window?.makeKeyAndOrderFront(nil)
+        if let frameDescriptor = frameDescriptor {
+            window.setFrame(from: frameDescriptor)
+        }
+
+        window.makeKeyAndOrderFront(nil)
+        self.window = window
 
         NotificationCenter.default.addObserver(self, selector: .appWindowWillClose,
                                                name: NSWindow.willCloseNotification,
-                                               object: wc.window)
+                                               object: window)
     }
 }
 
 // MARK: - Notifications
 extension AppController {
     @objc func appWindowWillClose(notification: Notification) {
-        guard let window = notification.object as? NSWindow else { return }
+        guard let window = notification.object as? NSWindow, window == self.window else { return }
 
         NotificationCenter.default.removeObserver(self, name: NSWindow.willCloseNotification,
                                                   object: window)
-        self.windowControllers.removeAll { window == $0.window }
+
+        let frameDescriptor = window.frameDescriptor
+        UserDefaults.standard.setValue(frameDescriptor, forKey: "CameraWindowFrame")
+        self.window = nil
     }
 }
